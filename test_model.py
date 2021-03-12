7820bd6012aa2d038161f42edf7a441ba01b9589
Petar <petar.sarajcev@fesb.hr>
2021-03-12 14:31:52 +0100

    Add tests.

--- test_model.py
+++ test_model.py
@@ -1,4 +1,17 @@
+"""
+Test model.py
+"""
+
+
 def test_exponential_sample_weights():
-    """ Test xponential_sample_weights function. """
+    """ Test exponential_sample_weights function. """
     from model import exponential_sample_weights
-    assert exponential_sample_weights(1) == [1.]
+    from numpy.testing import assert_array_almost_equal
+
+    weights_one = exponential_sample_weights(1)
+    weights_two = exponential_sample_weights(2)
+    weights_three = exponential_sample_weights(3)
+
+    assert weights_one == [1.]
+    assert_array_almost_equal(weights_two, [1., 0.367879], decimal=4)
+    assert_array_almost_equal(weights_three, [1., 0.606530, 0.367879], decimal=4)
